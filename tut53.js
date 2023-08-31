ae817e954e13536f7c2d5bcc7516d26a511528ea
Ashish <143717187+Krashish07@users.noreply.github.com>
2023-08-31 23:14:43 +0530

    Add files via upload

---
+++ tut53.js
@@ -0,0 +1,32 @@
+// console.log("This is a uniques Javascript")
+
+function greet(name, greetText) {
+    console.log(greetText + "" + name)
+    console.log(name + " is a Good boy ")
+}
+let name = "Harry"
+let name1 = "Shubham"
+let name2 = "Ankit"
+let name3 = "Dimple"
+let greetText = " Good Morning "
+
+function sum(a,b,c) {
+    let d = a+b+c
+    return  d;
+    
+}
+
+greet(name, greetText)
+greet(name1, greetText)
+greet(name2, greetText)
+greet(name3, greetText)
+
+
+
+// console.log(name);
+// console.log(name2)
+// console.log(name3)
+// console.log(name4)
+let returnval= sum(1,2,3,)
+console.log(returnval)
+
